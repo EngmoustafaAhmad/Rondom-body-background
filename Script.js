edef42fd2487b3c751257a7f087c4f82625969d5
EngmoustafaAhmad <moustafaahmed51015@gmail.com>
2024-12-01 23:30:00 +0200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,5 @@
+let btn = document.querySelector("#btn");
+btn.addEventListener('click',randombg);
+function randombg(){
+    document.body.style.backgroundColor='#'+Math.random().toString(16).slice(2,8);
+}
